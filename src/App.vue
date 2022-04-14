06c94dd6652138450325cd86352d5ced11d13d02
rafischer1 <artiefischer@gmail.com>
2022-04-14 14:37:08 -0600

    fix: set own image as bg image

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,12 @@
 
   <!-- Main Content -->
   <section class="container mx-auto">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+    <div
+      class="bg-white/[.9] rounded border border-gray-200 relative flex flex-col mt-20"
+    >
+      <div
+        class="px-6 pt-6 pb-5 font-bold border-b border-gray-200 bg-white/[.95]"
+      >
         <span class="card-title">Tea First Selections</span>
         <!-- Icon -->
         <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,23 +1,28 @@
 <template>
   <!-- Introduction -->
-  <section class="mb-8 py-20 text-white text-center relative">
-    <div
-      class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-    ></div>
-    <div class="container mx-auto">
-      <div class="text-white main-header-content">
-        <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
-        <p class="w-full md:w-8/12 mx-auto">
-          Tea First is an independent music label bring you the best
-          experimental music composition in the galaxy. <br />
-          Tea First, Then Be Ready...
-        </p>
-      </div>
+  <section class="mb-8 py-20 text-white text-center static">
+    <div class="absolute inset-0 w-full h-full bg-contain introduction-bg">
+      <img
+        class="relative block mx-auto mt-5 w-auto h-7/8"
+        src="assets/img/tea_first.png"
+        alt="tf-logo"
+      />
     </div>
-    <img
-      class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-      src="assets/img/introduction-music.png"
-    />
+    <!--    <div class="container mx-auto">-->
+    <!--      <div class="text-white main-header-content">-->
+    <!--        <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>-->
+    <!--        <p class="w-full md:w-8/12 mx-auto">-->
+    <!--          Tea First is an independent music label bring you the best-->
+    <!--          experimental music composition in the galaxy. <br />-->
+    <!--          Tea First, Then Be Ready...-->
+    <!--        </p>-->
+    <!--      </div>-->
+    <!--    </div>-->
+    <!--    <img-->
+    <!--      class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"-->
+    <!--      src="assets/img/introduction-music.png"-->
+    <!--      alt="music"-->
+    <!--    />-->
   </section>
 </template>
 
