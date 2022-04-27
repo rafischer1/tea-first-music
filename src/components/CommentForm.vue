f21195c2a013cd7323b1ae3d621af8aefa142dce
rafischer1 <artiefischer@gmail.com>
2022-04-27 10:32:37 -0600

    feat: creates CommentForm component

---
+++ src/components/CommentForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container mx-auto mt-6">
+    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+        <!-- Comment Count -->
+        <span class="card-title">Comments {{ track?.comment_count }}</span>
+        <i class="fa fa-comments float-right text-teal-600 text-2xl"></i>
+      </div>
+      <div class="p-6">
+        <form>
+          <textarea
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
+            placeholder="Your comment here..."
+          ></textarea>
+          <button
+            type="submit"
+            class="py-1.5 px-3 rounded text-white bg-teal-600 block"
+          >
+            Submit
+          </button>
+        </form>
+        <!-- Sort Comments -->
+        <select
+          class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        >
+          <option value="1">Latest</option>
+          <option value="2">Oldest</option>
+        </select>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CommentForm",
+});
+</script>
--- src/views/ViewTrack.vue
+++ src/views/ViewTrack.vue
@@ -8,6 +8,11 @@
       <!-- Play/Pause Button -->
       <button
         type="button"
+        @click.prevent.stop="play"
+        :class="{
+          'text-orange-400 border-2 border-orange-400 hover:text-orange-400 hover:border-orange-400':
+            activePlay,
+        }"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none hover:text-teal-800 hover:border-2 hover:border-teal-800 active:text-teal-600 active:border-2 active:border-teal-600"
       >
         <i class="fas fa-play"></i>
@@ -20,36 +25,7 @@
     </div>
   </section>
   <!-- Form -->
-  <section class="container mx-auto mt-6">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <!-- Comment Count -->
-        <span class="card-title">Comments {{ track?.comment_count }}</span>
-        <i class="fa fa-comments float-right text-teal-600 text-2xl"></i>
-      </div>
-      <div class="p-6">
-        <form>
-          <textarea
-            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
-            placeholder="Your comment here..."
-          ></textarea>
-          <button
-            type="submit"
-            class="py-1.5 px-3 rounded text-white bg-teal-600 block"
-          >
-            Submit
-          </button>
-        </form>
-        <!-- Sort Comments -->
-        <select
-          class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        >
-          <option value="1">Latest</option>
-          <option value="2">Oldest</option>
-        </select>
-      </div>
-    </div>
-  </section>
+  <CommentForm />
   <!-- Comments -->
   <ul class="container mx-auto">
     <li class="p-6 bg-gray-50 border border-gray-200">
@@ -63,13 +39,17 @@ import { defineComponent } from "vue";
 import RafComment from "@/components/comment.vue";
 import { songsCollection } from "@/includes/firebase";
 import Track from "@/interfaces/song.interface";
+import CommentForm from "@/components/CommentForm.vue";
 
 export default defineComponent({
   name: "ViewTrack",
-  components: { RafComment },
+  components: { CommentForm, RafComment },
   data() {
     return {
       track: null as unknown as Track,
+      activePlay: false,
+      playButton:
+        "z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none hover:text-teal-800 hover:border-2 hover:border-teal-800 active:text-teal-600 active:border-2 active:border-teal-600",
     };
   },
   async created() {
@@ -82,6 +62,17 @@ export default defineComponent({
       }
       this.track = snapshot.data() as Track;
     }
+  },
+  methods: {
+    play() {
+      this.toggleActivePlay();
+      setTimeout(() => {
+        this.toggleActivePlay();
+      }, 2000);
+    },
+    toggleActivePlay() {
+      this.activePlay = !this.activePlay;
+    },
   },
 });
 </script>
