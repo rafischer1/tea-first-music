d204c68bbb2eea49f680f57a0c1fc6d92ffefc9f
rafischer1 <artiefischer@gmail.com>
2022-04-27 10:40:27 -0600

    wip: commenting logic setup and form

--- src/components/CommentForm.vue
+++ src/components/CommentForm.vue
@@ -7,18 +7,24 @@
         <i class="fa fa-comments float-right text-teal-600 text-2xl"></i>
       </div>
       <div class="p-6">
-        <form>
-          <textarea
+        <vee-form :validation-schema="schema" @submit="submitComment">
+          <vee-field
+            type="textarea"
+            name="textarea"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
             placeholder="Your comment here..."
-          ></textarea>
+          ></vee-field>
           <button
+            :disabled="success"
             type="submit"
             class="py-1.5 px-3 rounded text-white bg-teal-600 block"
           >
             Submit
           </button>
-        </form>
+          <ErrorMessage>Get out of here!!!</ErrorMessage>
+          <div v-show="success" class="text-teal-600">Commenting!....</div>
+        </vee-form>
+
         <!-- Sort Comments -->
         <select
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
@@ -33,8 +39,37 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import {
+  defineRule,
+  ErrorMessage,
+  Field as VeeField,
+  Form as VeeForm,
+} from "vee-validate";
+import { max, min, required } from "@vee-validate/rules";
+
+defineRule("required", required);
+defineRule("min", min);
+defineRule("max", max);
 
 export default defineComponent({
   name: "CommentForm",
+  components: { VeeForm, VeeField, ErrorMessage },
+  data() {
+    return {
+      success: false,
+      schema: {
+        textarea: "required|min:3|max:100",
+      },
+    };
+  },
+  methods: {
+    submitComment(values: any) {
+      console.log("Comment:", values);
+      this.$data.success = true;
+      setTimeout(() => {
+        this.$data.success = false;
+      }, 3000);
+    },
+  },
 });
 </script>
