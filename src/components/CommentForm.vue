81e1b9ef8fe407090e2030040f981da18e7b0273
rafischer1 <artiefischer@gmail.com>
2022-04-29 10:36:29 -0600

    feat: icon directive

--- src/components/CommentForm.vue
+++ src/components/CommentForm.vue
@@ -1,12 +1,14 @@
 <template>
   <section class="container mx-auto mt-6">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+      <div
+        class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+        v-icon:full="icon"
+      >
         <!-- Comment Count -->
         <span class="card-title text-zinc-700"
           >Comments ({{ commentCount }})</span
         >
-        <i class="fa fa-comments float-right text-teal-600 text-2xl"></i>
       </div>
       <div class="p-6">
         <vee-form :validation-schema="schema" @submit="submitComment">
@@ -65,6 +67,7 @@ export default defineComponent({
       schema: {
         comment: "required|min:3|max:140",
       },
+      icon: "fa fa-comments float-right text-teal-600 text-2xl",
     };
   },
   methods: {
--- src/directives/icon.directive.ts
+++ src/directives/icon.directive.ts
@@ -1,6 +1,30 @@
+import { DirectiveBinding } from "vue";
+
 export default {
-  beforeMount(el: HTMLElement) {
-    el.innerHTML +=
-      "<i class='fa fa-headphones-alt float-right text-teal-600 text-xl'></i>";
+  beforeMount(el: HTMLElement, binding: DirectiveBinding<any>) {
+    let iconClass = "";
+    binding.arg === "full"
+      ? (iconClass = binding.value)
+      : (iconClass = `fa fa-${binding.value}`);
+
+    binding.modifiers
+      ? binding.modifiers.right
+        ? (iconClass += " float-right ")
+        : iconClass
+      : iconClass;
+
+    binding.modifiers
+      ? binding.modifiers.teal
+        ? (iconClass += " text-teal-600 ")
+        : iconClass
+      : iconClass;
+
+    binding.modifiers
+      ? binding.modifiers.xl
+        ? (iconClass += " text-xl ")
+        : iconClass
+      : iconClass;
+
+    el.innerHTML += `<i class="${iconClass}"></i>`;
   },
 };
