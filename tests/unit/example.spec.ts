2724cee64ee2d72fb3e2e7e50a3e593c26521084
rafischer1 <artiefischer@gmail.com>
2022-04-29 22:32:51 -0600

    feat: basic test setup

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,4 @@
-// import { shallowMount } from "@vue/test-utils";
-// import HelloWorld from "@/components/HelloWorld.vue";
-//
-// describe("HelloWorld.vue", () => {
-//   it("renders props.msg when passed", () => {
-//     const msg = "new message";
-//     const wrapper = shallowMount(HelloWorld, {
-//       props: { msg },
-//     });
-//     expect(wrapper.text()).toMatch(msg);
-//   });
-// });
+test("Sanity Test: 2 + 3 === 5", () => {
+  const x = 2 + 3;
+  expect(x).toEqual(5);
+});
